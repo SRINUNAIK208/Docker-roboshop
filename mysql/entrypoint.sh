81705b13cef99adcdf420eb105d50e67e69c0924
SRINUNAIK208 <srinaik208@gmail>
2026-03-29 07:47:58 +0530

    rbac-v2

---
+++ mysql/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if[ -f /tmp/mysql_root_password ]; then
+ PASSWORD=$(cat /tmp/mysql_root_password)
+ echo "Accessed Root password"
+else
+ echo "password not found"
+ exit 1
+fi
+
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+
+exec /entrypoint.sh, mysqld
